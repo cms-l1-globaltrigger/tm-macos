0ea25f0f84c3f9d6d5fa4a7cb186aaa1c0c6e017
Bernhard Arnold <bernhard.arnold@cern.ch>
2024-02-28 14:22:47 +0100

    added missing vars

--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-BOOST_BASE=$(pwd)/dist/boost
-XERCES_C_BASE=$(pwd)/dist/xerces-c
-UTM_BASE=$(pwd)/dist/utm
+: ${UTM_VERSION:=0.12.0}
+: ${BOOST_BASE:=$(pwd)/dist/boost}
+: ${XERCES_C_BASE:=$(pwd)/dist/xerces-c}
+: ${UTM_BASE:=$(pwd)/dist/utm}
+
 export REPAIR_LIBRARY_PATH=${UTM_BASE}/lib:${BOOST_BASE}/lib:${XERCES_C_BASE}/lib}
+
+# setup.py constraint of modules
+export UTM_ROOT=$(pwd)/build/utm-utm_${UTM_VERSION}
+export UTM_XSD_DIR=$UTM_BASE
 
 MODULES="tm-eventsetup tm-grammar tm-table"
 
